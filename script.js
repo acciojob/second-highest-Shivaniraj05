3f57da621959029f1a202f6a541936687b3af221
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-04 00:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+			//Write your code here
+			
+			let len=arr.length;
+			
+			if(len === 1 ||  len === 0)  
+			return "-Infinity";
+			
+			if(checkSame(arr))
+			return "-Infinity";
+			
+			
+			let max1=Math.max(arr[0], arr[1]), max2=Math.min(arr[0], arr[1]);
+			  
+			for(let i=2; i<len;i++){
+			    if(arr[i] > max1){
+			        max2=max1;
+			        max1=arr[i];
+			    }
+			    
+			    else if(arr[i] > max2){
+			        max2=arr[i];
+			    }
+			}
+			
+			return String(max2); 
+
+		}
+
+		function Main() {
+			var n = prompt("Enter the number of elements");
+			var arr = [];  
+			for (var i = 0; i < n; i++) {
+				arr[i] = prompt("Enter element " + (i+1));
+			}
+			alert(secondHighest(arr));
+		}
+		Main();
